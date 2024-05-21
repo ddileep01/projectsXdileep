0ee81a45fab7f4cc6701d059bcbe1b78134e592e
Dileep <115636822+ddileep01@users.noreply.github.com>
2024-05-21 17:54:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import header from './components/header';
+
+function App() {
+  return (
+    <div className="bg-[#111114] h-screen">
+      <header/>
+    </div>
+  );
+}
+
+export default App;
